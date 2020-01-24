8298373a490e6e11ab8b7dae8109a783d4609144
Mweber10 <57111116+Mweber10@users.noreply.github.com>
2020-01-24 13:17:51 -0700

    Add files via upload

--- Teensy/main.cpp
+++ Teensy/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
-
-//int led = 13;
+// #include <SPIFlash.h>
+#include <SerialFlash.h>
+#include <SPI.h>
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,41 +10,46 @@ void setup() {
       double threshold = 1.435; //volts  // int/double threshold_voltage - from testing with photodiode
       // Photodiode input voltage - *input*
       double input_voltage = 3.30; //volts //double - required input voltage for photodiode  
-      // Attitude data (simulated) - *Science Data Storage*
       // Packetized data (?) - *Data Transmission*
-        //digitalWrite()
   // Initialize pin modes (digital, analog, Tx, Rx, etc)
-  pinMode(2, INPUT); //pin for receiving data from ADC
-  pinMode(1, CORE_TXD0_PIN); //pin for transmitting data to NOR Flash mem
-  pinMode(8, PIN_SPI_SS);    //pin 8 for Slave Select    //connected to pin 1 of flash mem 1
-  pinMode(9, PIN_SPI_SS);    //pin 9 for Slave Select    //connected to pin 1 of flash mem 2
-  pinMode(10, PIN_SPI_SS);   //pin 10 for Slave Select   //connected to pin 3 on ADC (ADC_CS)
+
+  SPI.begin();
+  pinMode(8, OUTPUT);    //pin 8 for Slave Select    //connected to pin 1 of flash mem 1 - output from master
+  pinMode(9, OUTPUT);    //pin 9 for Slave Select    //connected to pin 1 of flash mem 2 - output from master
+  pinMode(10, INPUT);   //pin 10 for Slave Select    //connected to pin 3 on ADC (ADC_CS) - input to master
+  pinMode(11, PIN_SPI_MOSI); //pin 11 for Master Output Slave Input (MOSI) - sends data to flash memory
   pinMode(12, PIN_SPI_MISO); //pin 12 for Master Input Slave Output (MISO) - receives data from ADC //connects to pin 6 on ADC
   pinMode(13, PIN_SPI_SCK);  //pin 13 for Serial Clock (SCK) - output from master //connects to pin 5 on ADC
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-boolean condish = true;
-while (condish)
+// put your main code here, to run repeatedly:
+boolean dataCapture = true;
+while (dataCapture)
 {
 // SUN-THRESHOLD MEASUREMENT
-    // If photodiode voltage > threshold
-      // do nothing (keep taking data)
-      // remain pinMode(~,input)
-    // else
-      // stop taking data - break loop
-      // condish = false;
+  // If photodiode voltage > threshold
+    // do nothing (keep taking data)
+    // remain pinMode(~,input)
+  // else
+    // stop taking data - break loop
+    // dataCapture = false;
  
 // SCIENCE DATA STORAGE (which pin inputs/outputs this data?)
     // take variable for digital data from ADC
-    // take variable for simulated telemetry data (SPI from computer?)
-    // append these datas together (??????)
+    digitalWrite(10,LOW);
+
     // write to NOR flash chips (??????)
+    SPI.beginTransaction(SPISettings(1000000,MSBFIRST,SPI_MODE3)); //SPISettings(maxSpeed,dataOrder,dataMode)
+    digitalWrite(8,LOW); // set Slave Select pin 8 to low to select chip
+    SPI.transfer(data); //write data to chip
+    digitalWrite(8,HIGH); // set Slave Select pin 8 to high to de-select chip
+    SPI.endTransaction();
 
 // DATA TRANSMISSION
     // Take stored data from NOR flash memory
+    
     // packetize data for transmission (???????)
-    // Serial data output to simulated ground station
+    // Serial data output to laptop
 }
 }
